6052c15082aea34b6b84dabbbb2d67e1a4efd24a
Liberto7045 <124561906+Liberto7045@users.noreply.github.com>
2026-05-18 16:59:15 +0200

    Update restart_cluster.sh to restart Docker and deploy stack

--- Nodo-Control/ansible_project/restart_cluster.sh
+++ Nodo-Control/ansible_project/restart_cluster.sh
@@ -1,7 +1,8 @@
-k#!/bin/bash
+#!/bin/bash
 echo "Reiniciando Docker en todos los nodos..."
-ansible swarm_nodes -i ~/ansible_project/hosts -m shell -a "sudo systemctl >
-sleep 25
+ansible swarm_nodes -i ~/ansible_project/hosts -m shell -a "sudo systemctl restart docker" -b
+sleep 15
 echo "Redesplegando stack..."
-ssh swarmmanager@192.168.56.11 "sudo docker stack rm mi_proyecto && sleep 1>
+ssh swarmmanager@192.168.56.11 "sudo docker stack deploy -c /home/swarmmanager/stack.yml mi_proyecto"
 echo "Listo."
+
